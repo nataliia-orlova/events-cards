286ee5905413a8fadeecd0ce3ddefa5cea0c1180
Nataliia Orlova <nataliia-orlova@Nataliias-MacBook-Pro.local>
2023-04-20 18:13:08 +0200

    add build script

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/events-cards/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
 });
